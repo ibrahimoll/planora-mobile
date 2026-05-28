5d6199e80b2170e8156b2dd253aed4bd71bfec9d
ibrahimoll <ibrahimolleik3@gmail.com>
2026-05-28 11:58:14 +0300

    Support dark onboarding image assets

--- lib/features/onboarding/models/onboarding_page_data.dart
+++ lib/features/onboarding/models/onboarding_page_data.dart
@@ -8,6 +8,7 @@ class OnboardingPageData {
   final String highlightedText;
   final String description;
   final String imageAsset;
+  final String? darkImageAsset;
   final IconData? icon;
 
   const OnboardingPageData({
@@ -16,8 +17,19 @@ class OnboardingPageData {
     this.highlightedText = '',
     required this.description,
     required this.imageAsset,
+    this.darkImageAsset,
     this.icon,
   });
+
+  String imageAssetFor(BuildContext context) {
+    final isDark = Theme.of(context).brightness == Brightness.dark;
+
+    if (isDark && darkImageAsset != null) {
+      return darkImageAsset!;
+    }
+
+    return imageAsset;
+  }
 
   bool get isIntro => type == OnboardingPageType.intro;
   bool get isFeature => type == OnboardingPageType.feature;
