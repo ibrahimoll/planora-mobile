7b885a3d726157535f8bd73e359f8c41493ed2e4
ibrahimoll <ibrahimolleik3@gmail.com>
2026-05-28 11:17:55 +0300

    Make onboarding dots theme aware

--- lib/features/onboarding/widgets/onboarding_page_dot.dart
+++ lib/features/onboarding/widgets/onboarding_page_dot.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
-import '../../../core/theme/planora_theme.dart';
-
 class OnboardingPageDot extends StatelessWidget {
   final PageController controller;
   final int count;
@@ -15,14 +13,16 @@ class OnboardingPageDot extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+
     return SmoothPageIndicator(
       controller: controller,
       count: count,
       effect: ExpandingDotsEffect(
         dotHeight: 8,
         dotWidth: 8,
-        activeDotColor: PlanoraTheme.primaryPurple,
-        dotColor: PlanoraTheme.border,
+        activeDotColor: colorScheme.primary,
+        dotColor: colorScheme.outline,
         spacing: 8,
         radius: 100,
         expansionFactor: 3.4,
