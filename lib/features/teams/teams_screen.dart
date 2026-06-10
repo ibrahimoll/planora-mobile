3996676b4ad3090274f4638747eac44b84194326
ibrahimoll <ibrahimolleik3@gmail.com>
2026-06-10 11:18:45 +0300

    Fix broken teams screen file

--- lib/features/teams/teams_screen.dart
+++ lib/features/teams/teams_screen.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
 class TeamsScreen extends StatelessWidget {
-  final dynamic teamsApi, projectsApi, tasksApi;
+  final Object? teamsApi;
+  final Object? projectsApi;
+  final Object? tasksApi;
   final bool showBackButton;
   final VoidCallback? onTeamsChanged;
-
   const TeamsScreen({super.key, this.teamsApi, this.projectsApi, this.tasksApi, this.showBackButton = false, this.onTeamsChanged});
-
   @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(automaticallyImplyLeading: showBackButton,
+  Widget build(BuildContext context) {
+    return const
